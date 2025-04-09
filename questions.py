f4435a2ec7ac1245b700b96647cc39ec106efa1f
catomaior <pietro.firpo@pm.me>
2025-04-09 17:35:53 +0200

    Clarify wording in a question for improved understanding in questions.py

--- questions.py
+++ questions.py
@@ -2,7 +2,7 @@ questions = [
     "If all roses are flowers and some flowers are red, can we deduce that some roses are red? Why?",
     "A brick weights 1 kg plus half of the weight of a brick. How much does the brick weight?",
     "If it rains, the ground will be wet. The ground is wet. Did it rain?",
-    "If you turn 90° left on your left three times in a row, which direction are you facing compared to your starting position?",
+    "If you turn 90° left on yourself three times in a row, which direction are you facing compared to your starting position?",
     "On a street there is a man who offers you a bet: He throws a coin and if it is tails you get $3. If it is heads you lose $1. You can play the game how many times you want. Should you start playing? Why?",
 	"You have a basket with 10 apples. You take away 3 apples. How many apples do you have?",
 	"If a train leaves the station at 3:00 PM traveling at 60 km/h and another train leaves the same station at 4:00 PM traveling at 80 km/h, at what time will the second train catch up to the first?",
