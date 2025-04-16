d27df5e3143823e9d3d9a3d252546dad658af5df
catomaior <pietro.firpo@pm.me>
2025-04-16 16:09:28 +0200

    Remove unused source file

--- models.py
+++
@@ -1,8 +0,0 @@
-supported_models = {
-    "gemma3-1b":
-    	"https://huggingface.co/unsloth/gemma-3-1b-it-GGUF/resolve/main/gemma-3-1b-it-Q4_K_M.gguf?download=true",
-    "gemma3-4b":
-    	"https://huggingface.co/unsloth/gemma-3-4b-it-GGUF/resolve/main/gemma-3-4b-it-Q4_K_M.gguf?download=true",
-    "granite3.2-2b":
-    	"https://huggingface.co/ibm-research/granite-3.2-2b-instruct-GGUF/resolve/main/granite-3.2-2b-instruct-Q4_K_M.gguf?download=true"
-}
